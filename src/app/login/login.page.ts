de9633be2523eb035c80d9abc1a1287a1325dfb8
boybalkan <boybalkan97@gmail.com>
2020-09-17 14:29:27 +0200

    .

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { IUser } from '../User/user';
 
 @Component({
   selector: 'app-login',
@@ -7,14 +10,17 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor(private router: Router) { }
+  data: String;
+  error: String;
+  users: IUser[];
+  constructor(private router: Router,private http: HttpClient) { }
 
   ngOnInit() {
   }
 
   logMeIn(){
     this.router.navigate(['/home']);
+    
   }
   
 
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { IUser } from '../User/user';
 
 @Component({
   selector: 'app-register',
@@ -7,14 +10,30 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  data: String;
+  error: String;
+  users: IUser[];
+  constructor(private router: Router, private http: HttpClient) { 
+    this.data = '';
+   }
 
-  constructor(private router: Router) { }
-
-  ngOnInit() {
+  ngOnInit(): void {
+    this.http.get<IUser[]>("http://h2903840.stratoserver.net:49650/user").subscribe(result => {
+      this.users = result;
+    },
+    error => console.error(error)
+    );
   }
 
   signMeUp(){
     this.router.navigate(['/home']);
+  }
+
+  private prepareDataRequest(): Observable<object> {
+    // Define the data URL
+    const dataUrl = 'http://h2903840.stratoserver.net:49650/user/register';
+    // Prepare the request
+    return this.http.get(dataUrl);
   }
 
 }
