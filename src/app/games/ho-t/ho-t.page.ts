efdb607d0adb19a0b48ffed13d1bfc600dd6a06b
boybalkan <boybalkan97@gmail.com>
2020-09-26 22:47:16 +0200

    Completed HoT game

--- src/app/games/ho-t/ho-t.page.ts
+++ src/app/games/ho-t/ho-t.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/Router';
+import { format } from 'path';
 
 
 @Component({
@@ -13,21 +14,71 @@ export class HoTPage implements OnInit {
 
   ngOnInit() {
   }
+  k=0;
+  z=0;
+  radioValue: any;
+  inputBet: number ;
 
-  CallThrowCoin()
-{
-  if(Math.random() > 0.5){
-    //document.getElementById("coin").src  = "assets/kopf.png";
-    document.getElementById("ergebnis").innerHTML = "Kopf gewinnt!";
+  CallThrowCoin(){
+    var radioButtonSelect = this.radioValue;
+    var pot = this.inputBet;
+    var credit = parseFloat(document.getElementById("credit").innerHTML);
 
-
-  }else{
-    //document.getElementById("coin") = "assets/zahl.png";
-    document.getElementById("ergebnis").innerHTML = "Zahl gewinnt!";
-   
-  }
-   
-   
   
-}
+
+    
+  
+
+    if(Math.random() > 0.5){
+      //document.getElementById("coin").src  = "assets/kopf.png";
+      if(radioButtonSelect == "rBHead"){
+        document.getElementById("ergebnis").innerHTML = "... Head. You win!";
+        this.k = this.k + 1;
+        document.getElementById("wins").innerHTML = "Won: " + this.k ;
+
+        credit = +credit + +pot;
+        console.log(credit);
+        document.getElementById("credit").innerHTML = credit;
+
+      }else{
+        document.getElementById("ergebnis").innerHTML = "... Head. You lose!";
+        this.z = this.z + 1;
+        document.getElementById("loses").innerHTML = "Lost: " + this.z ;
+
+        credit = credit - pot;
+        console.log(credit);
+        document.getElementById("credit").innerHTML = credit;
+      }
+     
+   
+    }else{
+      //document.getElementById("coin") = "assets/zahl.png";
+      if(radioButtonSelect == "rBTail"){
+        document.getElementById("ergebnis").innerHTML = "... Tail. You win!";
+        this.k = this.k + 1;
+        document.getElementById("wins").innerHTML = "Won: " + this.k ;
+
+        credit = +credit + +pot;
+        console.log(credit);
+        document.getElementById("credit").innerHTML = credit;
+      }else{
+        document.getElementById("ergebnis").innerHTML = "... Tail. You lose!";
+        this.z = this.z + 1;
+        document.getElementById("loses").innerHTML = "Lost: " + this.z ;
+
+        credit = credit - pot;
+        console.log(credit);
+        document.getElementById("credit").innerHTML = credit;
+      }
+    
+    }
+  }
+
+  reset(){
+    document.getElementById("ergebnis").innerHTML = "And the winner is...";
+    document.getElementById("wins").innerHTML = "Won: ";
+    document.getElementById("loses").innerHTML = "Lost: ";
+    document.getElementById("bet").innerHTML = " ";
+    
+  }
 }
